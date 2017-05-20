aaa41c6f12619fadd2f57a1f2a62eb1041d5a779
cgars <christian@stuebeweg50.de>
2017-05-20 19:06:53 +0200

    Set knockout to non debug mode

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -13,13 +13,12 @@
             jquery: ["../lib/jquery/jquery"],
             "jquery-ui": ["../lib/jquery-ui/jquery-ui.min"],
             "jquery.ui.sortable": ["../lib/jquery-ui/jquery-ui"],
-            knockout: ["//cdnjs.cloudflare.com/ajax/libs/knockout/3.0.0/knockout-debug"],
+            knockout: ["../lib/knockout/knockout"],
             "ko.sortable": ["knockout-sortable.min"],
             bootstrap: ["../lib/bootstrap/js/bootstrap"],
             sammy: ["../lib/sammy/sammy"],
             moment: ["../lib/momentjs/moment"],
-            jsroutes: ["/jsroutes"],
-            datetimepicker: ["//cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.6.1/jquery-ui-timepicker-addon.min"]
+            jsroutes: ["/jsroutes"]
         }
     });
 
